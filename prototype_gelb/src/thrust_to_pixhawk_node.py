89a5ac8689d43f4ae9e9aaa2ce7f86079f2600b2
Noel Buehler <buehlern@ethz.ch>
2025-10-23 14:15:58 +0200

    switched mavlink message type

--- prototype_gelb/src/thrust_to_pixhawk_node.py
+++ prototype_gelb/src/thrust_to_pixhawk_node.py
@@ -1,7 +1,7 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float32
-from mavros_msgs.msg import ActuatorControl
+from mavros_msgs.msg import OverrideRCIn
 
 class ThrustToPixhawk(Node):
 
@@ -13,15 +13,23 @@ class ThrustToPixhawk(Node):
 
     def __init__(self):
         super().__init__('thrust_to_pixhawk')
-        self.actuator_publisher = self.create_publisher(ActuatorControl, '/mavros/actuator_control', 10)
+        self.actuator_publisher = self.create_publisher(OverrideRCIn, '/mavros/rc/override', 10)
         self.thrust_subscription = self.create_subscription(Float32, '/thrust_command', self.thrust_callback, 10)
 
     def thrust_callback(self, msg):
-        thrust_value = msg.data                     # Received thrust value
-        actuator_msg = ActuatorControl()                 # Create empty ActuatorControl message
-        actuator_msg.group_mix = 0                        # Set group mix (0 for main motors)
-        actuator_msg.controls[0] = thrust_value           # Set msg value to thrust value
-        self.actuator_publisher.publish(actuator_msg)     # Publish the actuator control message
+        thrust_value = msg.data
+        # Map thrust to PWM (assumes thrust in 0.0..1.0). Adjust ranges if different.
+        min_pwm = 800
+        max_pwm = 1900
+        normalized = max(0.0, min(1.0, float(thrust_value)))
+        pwm = int(min_pwm + normalized * (max_pwm - min_pwm))
+        rc_msg = OverrideRCIn()                 # Create empty OverrideRCIn message
+        rc_msg.channels = [0] * 8
+        rc_msg.channels[4] = pwm                 # Set the PWM value for the 5th channel
+        rc_msg.channels[5] = pwm                 # Set the PWM value for the 6th channel
+        rc_msg.channels[6] = pwm                 # Set the PWM value for the 7th channel
+        rc_msg.channels[7] = pwm                 # Set the PWM value for the 8th channel
+        self.actuator_publisher.publish(rc_msg)     # Publish the actuator control message
 
 def main():
     '''Main function to run the Thrust to Pixhawk ROS2 node'''
