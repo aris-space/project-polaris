e2a8db473799f54ee49c906bab6f9c2d6d313f6b
Noel Buehler <buehlern@ethz.ch>
2025-10-28 18:11:37 +0100

    added new test functions

--- prototype_gelb/src/test3.py
+++ prototype_gelb/src/test3.py
@@ -6,14 +6,13 @@ pi = pigpio.pi()
 if not pi.connected:
     print("Could not connect to pigpio daemon!")
     exit()
-
 pin = 18  # Example GPIO pin
+pi.set_servo_pulsewidth(pin, 1900)
 
-pi.write(pin, 1)  # Set pin high
-print("Pin set to HIGH")
+print("PWM signal set to 1900 microseconds")
 time.sleep(5)  # Wait for 5 seconds
-print("waiting for done.")
+print("waiting done.")
 
-pi.write(pin, 0)  # Set pin low
-print("Pin set to LOW")
+pi.set_servo_pulsewidth(pin, 0)  # stop pulses
+print("PWM signal stopped")
 pi.stop()
