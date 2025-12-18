2e5ae8e37be247929c64562d5369832183d13ec3
ridhchoudhury <ridh.choudhury@gmail.com>
2025-12-18 13:01:50 +0000

    added ice_measurement executable in setup

--- src/measurement/ping_sonar/setup.py
+++ src/measurement/ping_sonar/setup.py
@@ -1,29 +1,27 @@
 from setuptools import find_packages, setup
 
-package_name = 'ping_sonar'
+package_name = "ping_sonar"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='root',
-    maintainer_email='ridh.choudhury@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer="root",
+    maintainer_email="ridh.choudhury@gmail.com",
+    description="TODO: Package description",
+    license="TODO: License declaration",
     extras_require={
-        'test': [
-            'pytest',
+        "test": [
+            "pytest",
         ],
     },
     entry_points={
-        'console_scripts': [
-        ],
+        "console_scripts": ["ice_measurement = ping_sonar.ice_measurement:main"],
     },
 )
