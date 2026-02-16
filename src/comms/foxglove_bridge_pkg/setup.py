bc44cf953a70c4ec6ea79c6f7993cd58950b4e56
Noel Buehler <buehlern@student.ethz.ch>
2026-02-16 15:51:56 +0000

    fixed launch files entry points for foxglove and mode control

--- src/comms/foxglove_bridge_pkg/setup.py
+++ src/comms/foxglove_bridge_pkg/setup.py
@@ -25,6 +25,8 @@ setup(
         ],
     },
     entry_points={
-        "console_scripts": [],
+        "console_scripts": [
+            "foxglove_bridge = foxglove_bridge.foxglove_bridge:main"
+        ],
     },
 )
