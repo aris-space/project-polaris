4fb7d3c33e44e1adc727cff1149072876e69b46c
Noel Buehler <buehlern@student.ethz.ch>
2026-02-16 15:37:55 +0000

    added an entry point for the mode control pkg

--- src/controller/mode_control_pkg/setup.py
+++ src/controller/mode_control_pkg/setup.py
@@ -1,4 +1,5 @@
 from setuptools import find_packages, setup
+from glob import glob
 
 package_name = 'mode_control_pkg'
 
@@ -7,9 +8,9 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        ("share/" + package_name + "/launch", glob("launch/*.py")),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -23,7 +24,7 @@ setup(
         ],
     },
     entry_points={
-        'console_scripts': [
+        'console_scripts': ['mode_control = mode_control_pkg.mode_control_node:main'
         ],
     },
 )
