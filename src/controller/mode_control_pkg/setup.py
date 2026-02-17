6ee44eba9372cd8a412e7642ec9d9d843b2e1b32
Gleb Platonov <glebpl.04@web.de>
2026-02-17 10:22:29 +0100

    added the manual altitude hold control node

--- src/controller/mode_control_pkg/setup.py
+++ src/controller/mode_control_pkg/setup.py
@@ -25,6 +25,7 @@ setup(
     entry_points={
         'console_scripts': [
             'manual_control_node = mode_control_pkg.manual_control_node:main',
+            'manual_altitude_hold_control_node = mode_control_pkg.manual_altitude_hold_control_node:main',
         ],
     },
 )
