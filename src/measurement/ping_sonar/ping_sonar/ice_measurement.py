dc63673cb2b574e122942cba4bd4771a24fd33ff
ridhchoudhury <ridh.choudhury@gmail.com>
2026-02-20 17:28:06 +0100

    added small delay to help with init fialures

--- src/measurement/ping_sonar/ping_sonar/ice_measurement.py
+++ src/measurement/ping_sonar/ping_sonar/ice_measurement.py
@@ -8,6 +8,7 @@ from datetime import datetime
 from std_msgs.msg import String
 from config_pkg.constants import Logs, Comms
 from rcl_interfaces.msg import SetParametersResult
+import time
 
 
 class Ice_Measurement(Node):
@@ -22,7 +23,7 @@ class Ice_Measurement(Node):
         self.ping.connect_serial(
             Comms.PING_SONAR_PORT, Comms.USB0_BAUD_RATE
         )  # specifies relevant port
-
+        time.sleep(0.5)
         self.initialization = self.ping.initialize()
 
         if not self.initialization:
