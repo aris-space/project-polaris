0df6f2d54093e2f7927d81435ae8b09d7677ab96
ridhchoudhury <ridh.choudhury@gmail.com>
2026-02-24 10:25:30 +0100

    attempted bugfixes to collision avoidance logic

--- src/controller/mode_control_pkg/mode_control_pkg/collision_avoidance.py
+++ src/controller/mode_control_pkg/mode_control_pkg/collision_avoidance.py
@@ -14,6 +14,9 @@ class CollisionAvoidanceNode(Node):
         self.checking = (
             True  # Flag to indicate if we are currently checking for distance
         )
+        self.manual_mode_published = (
+            False  # Flag to track if manual mode has been published
+        )
         self.rearm_distance = (
             0.07  # Distance threshold for rearming the system after an emergency stop
         )
@@ -74,23 +77,41 @@ class CollisionAvoidanceNode(Node):
     def rearm_callback(self):
         time_elapsed = (self.get_clock().now() - self.trigger_time).nanoseconds / 1e9
         if time_elapsed >= self.rearm_delay:
+
             if self.distance >= self.rearm_distance:
-                self.checking = True
+
+                if not self.checking:
+                    self.checking = True
+                    self.get_logger().info("Collision avoidance system rearmed.")
+
                 self.rearm_timer.cancel()
                 self.rearm_timer = None
-                self.get_logger().info("Collision avoidance system rearmed.")
 
     def distance_cb(self, msg):
         self.distance = msg.data
         # self.get_logger().info(f'Distance: {self.distance}')
+        if not self.checking:
+
+            if not self.manual_mode_published:
+                self.publish_mode("MANUAL")
+                self.get_logger().info(
+                    "Manual override active: System mode set to MANUAL."
+                )
+                self.manual_mode_published = True
+        else:
+            # Resets the latch automatically when checking becomes True again
+            self.manual_mode_published = False
 
         if self.distance < self.trigger_distance and self.checking:
+
             # Trigger emergency stop: same as mode_control_node
             if self.current_mode != "emergency_stop":
                 self.current_mode = "emergency_stop"
                 self.pixhawk_mode = "MANUAL"
                 self.publish_mode("emergency_stop")
-                self.publish_pixhawk_mode("MANUAL")
+                self.publish_pixhawk_mode(
+                    "MANUAL"
+                )  # TODO: once we are able to implement POS_HOLD, switch to that mode
 
             if self.rearm_timer is None:
                 self.trigger_time = self.get_clock().now()
