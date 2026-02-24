327b7fe90cff7ff58b2fc4bf07b3a52efa2e76ff
Noel Buehler <buehlern@student.ethz.ch>
2026-02-24 11:03:34 +0000

    added publish_frequency_hz as rosparam, and adjusted to this in the launch files

--- src/sensors/ultrasonic_driver/launch/front.launch.py
+++ src/sensors/ultrasonic_driver/launch/front.launch.py
@@ -6,26 +6,29 @@ from config_pkg.constants import Ports
 
 
 def generate_launch_description():
-    serial_device = LaunchConfiguration('serial_device')
-    return LaunchDescription([
-        DeclareLaunchArgument(
-            'namespace',
-            default_value='default',
-            description='Namespace for the ultrasonic node'
-        ),
-        DeclareLaunchArgument(
-            'serial_device',
-            default_value=Ports.FRONT_ULTRASONIC_PORT,
-            description='Serial device path for the ultrasonic sensor'
-        ),
-        Node(
-            package='ultrasonic_driver',
-            executable='ultrasonic_node',
-            name='ultrasonic_sensor_node',
-            namespace='front',
-            output='screen',
-            parameters=[
-                {'serial_device': serial_device},
-            ],
-        ),
-    ])
+    serial_device = LaunchConfiguration("serial_device")
+    return LaunchDescription(
+        [
+            DeclareLaunchArgument(
+                "namespace",
+                default_value="default",
+                description="Namespace for the ultrasonic node",
+            ),
+            DeclareLaunchArgument(
+                "serial_device",
+                default_value=Ports.FRONT_ULTRASONIC_PORT,
+                description="Serial device path for the ultrasonic sensor",
+            ),
+            Node(
+                package="ultrasonic_driver",
+                executable="ultrasonic_node",
+                name="ultrasonic_sensor_node",
+                namespace="front",
+                output="screen",
+                parameters=[
+                    {"serial_device": serial_device},
+                    {"publish_frequency_hz": 20.0},
+                ],
+            ),
+        ]
+    )
