4cd1123aa9d54990f0fa676526e2b25ba0f683dc
ridhchoudhury <ridh.choudhury@gmail.com>
2026-03-03 12:56:56 +0100

    fixed setup and launch file

--- src/sensors/temperature_sensor_pkg/setup.py
+++ src/sensors/temperature_sensor_pkg/setup.py
@@ -27,7 +27,7 @@ setup(
     },
     entry_points={
         "console_scripts": [
-            "test_temp = temperature_sensor_pkg.test_temp:main",
+            "test_temp = temperature_sensor_pkg.temperature_sensor_node:main",
         ],
     },
 )
