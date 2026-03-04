982013d29c7a76199cde39d8fc88da5e56c43c01
Linus-Mac <info@linus-kuster.ch>
2026-03-04 11:47:49 +0100

    a lot is wokring

--- top_station/Foxglove-extensions/POLARIS-Topc_Table-Foxglove/src/index.ts
+++ top_station/Foxglove-extensions/POLARIS-Topc_Table-Foxglove/src/index.ts
@@ -1,7 +1,10 @@
 import { ExtensionContext } from "@foxglove/extension";
 
-import { initExamplePanel } from "./ExamplePanel";
+import { initExamplePanel } from "./Main-panel";
 
 export function activate(extensionContext: ExtensionContext): void {
-  extensionContext.registerPanel({ name: "example-panel", initPanel: initExamplePanel });
+  extensionContext.registerPanel({
+    name: "polaris-ros-topic-tables",
+    initPanel: initExamplePanel,
+  });
 }
