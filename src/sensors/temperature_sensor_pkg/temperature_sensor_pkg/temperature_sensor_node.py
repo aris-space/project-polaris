b2574c4093f3a51101c51cc00fc4e881f888213d
paulzambelli <pzambelli@student.ethz.ch>
2026-03-07 18:36:57 +0100

    New If statement for when ESC will throttle down and new TODOs

--- src/sensors/temperature_sensor_pkg/temperature_sensor_pkg/temperature_sensor_node.py
+++ src/sensors/temperature_sensor_pkg/temperature_sensor_pkg/temperature_sensor_node.py
@@ -35,7 +35,7 @@ class Temperature_sensor(Node):
 
         self.get_logger().info(f"Successfully opened serial port {Ports.ARDUINO_PORT}")
 
-        self.timer = self.create_timer(0.01, self.timer_callback)
+        self.timer = self.create_timer(0.01, self.timer_callback) #TODO Lower Frequency?!
         self.buffer = bytearray()
         self.max_buffer_size = 1024  # Maximum buffer size to prevent overflow
 
@@ -79,11 +79,19 @@ class Temperature_sensor(Node):
                 "Temperatures [°C]: [%s]" % ", ".join(f"{v:.2f}" for v in values)
             )
 
+            #TODO: The Arduino should publish in the order below!!!
+            sensors_with_position = {0: "Back", 1: "Middle", 2: "Front"} # TODO: Add which sensor_i corresponds to which position in the Hardware
+
             for sensor_i in range(len(values)):
                 if values[sensor_i] > 26:  # TODO: Might need to be adapted
                     self.get_logger().warning(
-                        f"Sensor {sensor_i} is hot: {values[sensor_i]}°C"
-                    )  # TODO: Add which sensor_i corresponds to which position in the Hardware
+                        f"Sensor {sensor_i}; Position {sensors_with_position.get(sensor_i, "Unkown")} is hot and it will soon throttle down some ESCs: {values[sensor_i]}°C"
+                    )  
+
+                if values[sensor_i] >= 60:  # TODO: Might need to be adapted
+                    self.get_logger().warning(
+                        f"Sensor {sensor_i}; Position {sensors_with_position.get(sensor_i, "Unkown")} is too hot and throttles down some ESCs: {values[sensor_i]}°C"
+                    )  
 
             diag_msg = DiagnosticArray()
             diag_msg.header.stamp = self.get_clock().now().to_msg()
