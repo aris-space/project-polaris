2f11c4ca10ae067ffe486a316071da39b37ff6a1
Noel Buehler <buehlern@student.ethz.ch>
2026-03-09 10:09:15 +0000

    added the udev rules to the config constants file. + Edited cameras launch file

--- src/sensors/usb_cam_pkg/launch/launch_cameras.py
+++ src/sensors/usb_cam_pkg/launch/launch_cameras.py
@@ -1,6 +1,6 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-
+from config_pkg.constants import Ports
 
 def generate_launch_description():
     return LaunchDescription(
@@ -12,7 +12,7 @@ def generate_launch_description():
                 namespace="front",
                 parameters=[
                     {
-                        "video_device": "/dev/cam_front",
+                        "video_device": Ports.USB_CAM_FRONT_PORT,
                         "pixel_format": "mjpeg2rgb",  # The format that fixed the crash
                         "image_width": 640,
                         "image_height": 480,
@@ -26,7 +26,7 @@ def generate_launch_description():
                 namespace="tube",
                 parameters=[
                     {
-                        "video_device": "/dev/cam_tube",
+                        "video_device": Ports.USB_CAM_TUBE_PORT,
                         "pixel_format": "mjpeg2rgb",
                         "image_width": 640,
                         "image_height": 480,
