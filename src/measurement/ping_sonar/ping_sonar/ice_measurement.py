7caec3db300c7b024b005c0c7cb48bd1d71c92f0
ridhchoudhury <ridh.choudhury@gmail.com>
2026-03-09 17:14:25 +0100

    changed delay to 0.5 to debug

--- src/measurement/ping_sonar/ping_sonar/ice_measurement.py
+++ src/measurement/ping_sonar/ping_sonar/ice_measurement.py
@@ -37,7 +37,7 @@ class Ice_Measurement(Node):
         self.ping.connect_serial(
             Comms.PING_SONAR_PORT, Comms.USB0_BAUD_RATE
         )  # specifies relevant port
-        time.sleep(10)
+        time.sleep(0.5)
         self.initialization = self.ping.initialize()
 
         if not self.initialization:
