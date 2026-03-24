46f02cb75658f23dad50771f5597a20c41fc1927
Noel-ETHZ <buehlern@ethz.ch>
2026-03-24 17:07:05 +0000

    refined submodule setup file

--- setup_submodules.sh
+++ setup_submodules.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # 1. Initialize the submodule entry
 git submodule update --init --recursive
+echo "Initialized submodules."
 
 # 2. Apply the sparse-checkout logic
 cd src/comms/foxglove_bridge
 git sparse-checkout init --cone
 git sparse-checkout set ros
 git reset --hard HEAD
-
 echo "Foxglove Bridge submodule is ready at src/comms/foxglove_bridge/ros"
+
