27f24a186025066d9cb5f21591821b223dd1df0f
paulzambelli <paulzambelli@student.ethz.ch>
2026-03-26 08:25:32 +0100

    Start the keller_26x_node with the documentation of keller.

--- src/sensors/keller_26x_pkg/keller_26x_pkg/keller_26x_node.py
+++ src/sensors/keller_26x_pkg/keller_26x_pkg/keller_26x_node.py
@@ -6,16 +6,80 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
 
+from keller_protocol import keller_protocol as kp
+# use pip install keller-protocol
+# https://github.com/KELLERAGfuerDruckmesstechnik/keller_protocol_python
+
+
 
 class Keller26xNode(Node):
 
     def __init__(self):
         super().__init__('keller_26x_pressure')
+
+        self.bus = kp.KellerProtocol(port="COM17", baud_rate=9600, timeout=0.3, echo=True)
+        self.address = 1
+        self.serial_number = None
+        self.f73_channels = {
+            "CH0": 0,
+            "P1": 1,
+            "P2": 2,
+            "T": 3,
+            "TOB1": 4,
+            "TOB2": 5,
+            "ConTc": 10,
+            "ConRaw": 11,
+        }
+        self.init_f48()
+
         self.publisher_ = self.create_publisher(String, 'topic', 10)
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
 
+        
+    def init_f48(self) -> str:
+        """Initialise and release"""
+        answer = self.bus.f48(self.address)
+        print(f" Init of Device Address: {self.address} with Firmware: {answer}")
+
+    def get_serial(self) -> int:
+        """Get Serial Number from X-Line
+
+        :returns Serial Number
+        """
+        self.serial_number = self.bus.f69(self.address)
+        return self.serial_number
+
+    def get_address(self) -> int:
+        return self.address
+
+    def set_address(self, new_address: int) -> int:
+        """Change the Device address. -> Has to be unique on the RS485 bus
+
+        :param new_address: New address of the Device
+        :return: If successful return new_address otherwise old address and throw exception
+        """
+        self.address = self.bus.f66(self.address, new_address)
+        return self.address
+
+    def measure_tob1(self) -> float:
+        """Get temperature TOB1
+
+        :return: temperature
+        """
+        temperature = self.bus.f73(self.address, self.f73_channels["TOB1"])
+        return temperature
+
+    def measure_p1(self) -> float:
+        """Get pressure P1
+
+        :return: pressure
+        """
+        pressure = self.bus.f73(self.address, self.f73_channels["P1"])
+        return pressure
+    
+    
     def timer_callback(self):
         msg = String()
         msg.data = 'Hello World: %d' % self.i
