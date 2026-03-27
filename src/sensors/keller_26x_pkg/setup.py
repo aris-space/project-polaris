5609abad23ffe3af8f945752c2a786578bf222c1
paulzambelli <paulzambelli@student.ethz.ch>
2026-03-27 10:05:14 +0100

    Standard fixes in the node and launchfile. Deleted the config file..

--- src/sensors/keller_26x_pkg/setup.py
+++ src/sensors/keller_26x_pkg/setup.py
@@ -13,7 +13,6 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         ("share/" + package_name + "/launch", glob("launch/*.py")),
-        ("share/" + package_name + "/config", glob("config/*.yaml")),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
