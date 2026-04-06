acb0f920c61fb9f305dcd4caa3eb7ebe584f9b29
Linus-Mac <info@linus-kuster.ch>
2026-04-06 23:38:07 +0200

    heading made nicer

--- top_station/Foxglove-extensions/flight-indicators-POLARIS/src/instruments/HeadingIndicator.tsx
+++ top_station/Foxglove-extensions/flight-indicators-POLARIS/src/instruments/HeadingIndicator.tsx
@@ -4,6 +4,15 @@ import { ReactElement, useEffect, useRef } from "react";
 import { BundledFlightIndicators } from "../BundledFlightIndicators";
 
 type Props = { heading: number | undefined; size: string };
+const AIRPLANE_SCALE = 0.72;
+
+function removeHeadingCircle(el: HTMLDivElement): void {
+  const mechanicsBox = el.querySelector<HTMLElement>("div.instrument.heading div.mechanics");
+  if (!mechanicsBox) return;
+
+  const fiCircle = mechanicsBox.querySelector<HTMLElement>("img.box:not(.heading-airplane)");
+  fiCircle?.remove();
+}
 
 function setNorthUpHeading(el: HTMLDivElement, heading: number): void {
   const rose = el.querySelector<HTMLElement>("div.instrument.heading div.heading");
@@ -13,7 +22,7 @@ function setNorthUpHeading(el: HTMLDivElement, heading: number): void {
 
   const airplane = el.querySelector<HTMLElement>("div.instrument.heading img.heading-airplane");
   if (airplane) {
-    airplane.style.transform = `rotate(${heading}deg)`;
+    airplane.style.transform = `rotate(${heading}deg) scale(${AIRPLANE_SCALE})`;
     airplane.style.transformOrigin = "50% 50%";
   }
 }
@@ -26,6 +35,7 @@ export function HeadingIndicator({ heading, size }: Props): ReactElement {
     const el = containerRef.current;
     if (!el) return undefined;
     fi.current = new BundledFlightIndicators(el, FlightIndicators.TYPE_HEADING);
+    removeHeadingCircle(el);
     return () => {
       el.innerHTML = "";
       fi.current = null;
