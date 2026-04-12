33b2ae2f89c5e7edb9e98891b5aa6da613d61a79
Noel Buehler <buehlern@student.ethz.ch>
2026-04-12 16:28:53 +0000

    bugfix from last commit

--- src/sensors/usb_cam_pkg/launch/launch_cameras_opt.launch.py
+++ src/sensors/usb_cam_pkg/launch/launch_cameras_opt.launch.py
@@ -3,6 +3,7 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 from config_pkg.constants import Ports
 
+
 def generate_launch_description():
     # Helper to build the GStreamer string for Jetson Hardware Acceleration
     def get_gst_config_tube(device_path):
@@ -14,11 +15,11 @@ def generate_launch_description():
             "video/x-raw, format=BGRx ! "
             "videoconvert"
         )
-    
+
     def get_gst_config_front(device_path):
         return (
             f"v4l2src device={device_path} do-timestamp=true ! "
-            "image/jpeg, width=1920, height=1080 ! " 
+            "image/jpeg, width=1920, height=1080 ! "
             "nvv4l2decoder mjpeg=1 ! "
             "nvvidconv ! "
             "video/x-raw, format=BGR ! "
@@ -27,35 +28,41 @@ def generate_launch_description():
 
     # Camera 1 Node
     cam_front = Node(
-        package='gscam',
-        executable='gscam_main',
-        name='gscam_front',
-        namespace='front',
-        parameters=[{
-            'gscam_config': get_gst_config_front(Ports.USB_CAM_FRONT_PORT),
-            'camera_name': 'front_camera',
-            'frame_id': 'camera_front_link',
-            'image_encoding': 'rgb8',
-            'sync_sink': False # Helps with FPS stability
-        }]
+        package="gscam",
+        executable="gscam_node",
+        name="gscam_front",
+        namespace="front",
+        parameters=[
+            {
+                "gscam_config": get_gst_config_front(Ports.USB_CAM_FRONT_PORT),
+                "camera_name": "front_camera",
+                "frame_id": "camera_front_link",
+                "image_encoding": "rgb8",
+                "sync_sink": False,  # Helps with FPS stability
+            }
+        ],
     )
 
     # Camera 2 Node
     cam_tube = Node(
-        package='gscam',
-        executable='gscam_main',
-        name='gscam_tube',
-        namespace='tube',
-        parameters=[{
-            'gscam_config': get_gst_config_tube(Ports.USB_CAM_TUBE_PORT),
-            'camera_name': 'tube_camera',
-            'frame_id': 'camera_tube_link',
-            'image_encoding': 'rgb8',
-            'sync_sink': False
-        }]
+        package="gscam",
+        executable="gscam_node",
+        name="gscam_tube",
+        namespace="tube",
+        parameters=[
+            {
+                "gscam_config": get_gst_config_tube(Ports.USB_CAM_TUBE_PORT),
+                "camera_name": "tube_camera",
+                "frame_id": "camera_tube_link",
+                "image_encoding": "rgb8",
+                "sync_sink": False,
+            }
+        ],
     )
 
-    return LaunchDescription([
-        cam_front#,
-        #cam_tube
-    ])
+    return LaunchDescription(
+        [
+            cam_front  # ,
+            # cam_tube
+        ]
+    )
