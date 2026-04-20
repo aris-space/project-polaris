4708bb5a9eaf27148ef1815ed2af2d299fdcb320
ridhchoudhury <ridh.choudhury@gmail.com>
2026-04-20 18:12:19 +0200

    bash file now gives jetson logs feedback

--- start.sh
+++ start.sh
@@ -5,7 +5,8 @@ docker compose up -d
 
 echo "Waiting for build and launch to complete..."
 until docker exec jetson-container tmux has-session -t polaris 2>/dev/null; do
-    sleep 2
+    docker logs --tail 20 jetson-container 2>&1
+    sleep 3
 done
 
 docker exec -it jetson-container tmux attach -t polaris
