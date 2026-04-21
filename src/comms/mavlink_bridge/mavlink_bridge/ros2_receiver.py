c3205365a871cb160fcc19eaac31d9236e8c6863
paulzambelli <paulzambelli@student.ethz.ch>
2026-04-21 18:06:01 +0200

    Bugfix deleted something..

--- src/comms/mavlink_bridge/mavlink_bridge/ros2_receiver.py
+++ src/comms/mavlink_bridge/mavlink_bridge/ros2_receiver.py
@@ -334,6 +334,22 @@ class MavlinkBridgeReceiver(Node):
         )
 
     """--------------------------------------------- helper functions for the callback functions ---------------------------------------------"""
+    def send_4dof_command(self, control_input):
+        """
+        Input values: -1000 to 1000 (except heave, see below)
+        """
+        self._file_logger.info(
+            f"Sending 4DOF command with control input: {control_input}"
+        )
+        surge, sway, heave, yaw = control_input
+        self.port.mav.manual_control_send(
+            self.port.target_system,
+            int(surge),  # x: Forward/Back
+            int(sway),  # y: Left/Right
+            int(heave),  # z: Up/Down (range 0-1000, 500 is neutral)
+            int(yaw),  # r: Yaw
+            0,  # buttons bitmask
+        )
 
 
     def send_4dof_command_test(self, control_input):
