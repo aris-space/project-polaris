57088b112aa7794b850865a6fc006bc0722f38c9
paulzambelli <pzambelli@student.ethz.ch>
2026-05-16 18:50:54 +0200

    Removed orca_bringup.

--- src/autonomy/orca_bringup/scripts/disarm.sh
+++
@@ -1,2 +0,0 @@
-# Disarm Pixhawk via custom MAVLink bridge.
-ros2 topic pub --once /pixhawk/arm_cmd std_msgs/msg/Bool "{data: false}"
--- src/autonomy/orca_bringup/scripts/rov.sh
+++
@@ -1,2 +0,0 @@
-# Set Pixhawk mode for manual/ROV control via custom MAVLink bridge.
-ros2 topic pub --once /pixhawk/mode_cmd std_msgs/msg/String "{data: MANUAL}"
