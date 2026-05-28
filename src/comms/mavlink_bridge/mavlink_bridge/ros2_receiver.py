231b6a8e4e76d3c2dbd2d70f6ae8e26d4e8f5145
paulzambelli <paulzambelli@student.ethz.ch>
2026-05-28 11:38:39 +0200

    feature:NoVerticalMovement: heave=0.0.

--- src/comms/mavlink_bridge/mavlink_bridge/ros2_receiver.py
+++ src/comms/mavlink_bridge/mavlink_bridge/ros2_receiver.py
@@ -976,7 +976,7 @@ class MavlinkBridgeReceiver(Node):
             0.0,  # Position (ignored)
             surge, # surge
             0.0,
-            heave,  # heave
+            0.0,  # heave
             0.0,
             0.0,
             0.0,  # Acceleration (ignored)
